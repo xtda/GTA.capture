073c9263e1f4a94e4fdc5e6430128f7e7283500c
Andrew <xtda@users.noreply.github.com>
2018-03-10 01:31:39 +1100

    Hello World

---
+++ GTA.capture/MainWindow.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace GTA.capture
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.sysTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.sysTrayContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.screenshotsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sysTrayContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sysTrayIcon
+            // 
+            this.sysTrayIcon.ContextMenuStrip = this.sysTrayContextMenu;
+            this.sysTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("sysTrayIcon.Icon")));
+            this.sysTrayIcon.Visible = true;
+            // 
+            // sysTrayContextMenu
+            // 
+            this.sysTrayContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.screenshotsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.sysTrayContextMenu.Name = "sysTrayContextMenu";
+            this.sysTrayContextMenu.Size = new System.Drawing.Size(138, 54);
+            // 
+            // screenshotsToolStripMenuItem
+            // 
+            this.screenshotsToolStripMenuItem.Name = "screenshotsToolStripMenuItem";
+            this.screenshotsToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.screenshotsToolStripMenuItem.Text = "&Screenshots";
+            this.screenshotsToolStripMenuItem.Click += new System.EventHandler(this.screenshotsToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(134, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 145);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(412, 184);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(412, 184);
+            this.Name = "MainWindow";
+            this.ShowInTaskbar = false;
+            this.Text = "GTA Capture";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.sysTrayContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon sysTrayIcon;
+        private System.Windows.Forms.ContextMenuStrip sysTrayContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem screenshotsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+    }
+}
---
+++ GTA.capture/MainWindow.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace GTA.capture {
+    public partial class MainWindow : Form {
+        private readonly string _currentPath = Path.GetDirectoryName(Application.ExecutablePath);
+
+        public MainWindow() {
+            InitializeComponent();
+
+            Directory.CreateDirectory(_currentPath + "\\screenshots");
+
+            var screenShotHotKeyId = 0;
+            RegisterHotKey(Handle, screenShotHotKeyId, 0, Keys.F11.GetHashCode());
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+            UnregisterHotKey(Handle, 0);
+            Application.Exit();
+        }
+
+        private void screenshotsToolStripMenuItem_Click(object sender, EventArgs e) {
+            Process.Start(_currentPath + @"\screenshots");
+        }
+
+        protected override void WndProc(ref Message m) {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+                CaptureGtaScreen();
+        }
+
+        public void CaptureGtaScreen() {
+            var proc = Process.GetProcessesByName("notepad")[0];
+            var rect = new Rect();
+            GetWindowRect(proc.MainWindowHandle, ref rect);
+
+            var width = rect.right - rect.left;
+            var height = rect.bottom - rect.top;
+
+            var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            var graphics = Graphics.FromImage(bmp);
+            graphics.CopyFromScreen(rect.left, rect.top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
+
+            var date = DateTime.Now;
+            var month = date.ToString("MMMM");
+            ;
+            var filename = date.ToString("dd-MM-yyyy (HH-mm-ss)");
+            Directory.CreateDirectory(_currentPath + "\\screenshots\\" + month);
+            bmp.Save("screenshots\\" + month + "\\" + filename + ".png", ImageFormat.Png);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rect {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+    }
+}
